98b553eb3091d820ec820bb7205ab536b18a55c2
Ismael del Hoyo <ismaeldelhoyo@gmail.com>
2015-07-14 19:21:15 +0200

    Modulo 7 Ejercicio

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -9,16 +9,24 @@ exports.load = function(req,res,next,quizId){
 		}else{
 			next(new Error('No existe quizId=' + quizId));
 		}
-	}
-	).catch(function(error){next(error);})
+	}).catch(function(error){next(error);})
 };
 
 //GET /quizes
 exports.index =function(req,res){
-	models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/index.ejs',{ quizes: quizes});
+	if(req.query.search){
+		var buscar = ('%' + req.query.search + '%').replace(/ /g, '%');
+		models.Quiz.findAll({
+			where: ["pregunta like ?", buscar],
+			order: 'pregunta ASC'
+		}).then(function(quizes) {
+			res.render('quizes/index.ejs', {quizes: quizes});
+		}).catch(function(error) { next(error);})
+	}else{
+		models.Quiz.findAll().then(function(quizes){
+			res.render('quizes/index.ejs',{ quizes: quizes});
+		}).catch(function(error){next(error);})
 	}
-	).catch(function(error){next(error);})
 };
 
 //GET /quizes/:id
