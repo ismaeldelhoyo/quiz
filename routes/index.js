d19072486cf93fec6b7a073da835570e35f547ac
Ismael del Hoyo <ismaeldelhoyo@gmail.com>
2015-07-14 18:20:19 +0200

    Lista de preguntas

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,10 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizcontroller.question);
-router.get('/quizes/answer', quizcontroller.answer);
+//Definición de rutas de /quizes
+router.get('/quizes', quizcontroller.index);
+router.get('/quizes/:quizId(\\d+)', quizcontroller.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizcontroller.answer);
 
 router.get('/author', function(req, res) {
 	res.render('author');
